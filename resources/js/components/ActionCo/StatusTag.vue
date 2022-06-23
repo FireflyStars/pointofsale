e759fef81f1948ef140e5a01001b4140f41285af
Muhammad Imran <imranr970@gmail.com>
2022-06-23 14:43:00 +0500

    Fixed price and status tag issue

--- resources/js/components/ActionCo/StatusTag.vue
+++ resources/js/components/ActionCo/StatusTag.vue
@@ -50,6 +50,7 @@ const loaded = computed(() => store.getters[`${ACTION_COMMERCIAL_STATUS_MODULE}l
 
 
 watch(() => order_states, (current_val) => {
+    console.log("I was here in order_states")
     const order_state = current_val.value.find(obj => obj.id == props.id)
     if(typeof order_state != "undefined") {
         status.value = order_state.name
@@ -72,16 +73,18 @@ watch(() => props.id, (current_val) => {
 })
 
 onMounted(async ()=> {
+    console.log("I was onMOunted")
     if(loaded.value === false) {
-        await store.dispatch(`${ACTION_COMMERCIAL_STATUS_MODULE}${ORDERSTATETAG_LOAD_ORDER_STATES}`)
-        if(order_states.value.length > 0) {
-            const order_state = order_states.value.find(obj => obj.id == props.id)
-            if(typeof order_state != "undefined") {
-                status.value = order_state.name
-                style.value = `width:${props.width};background-color: ${order_state.color};color: ${order_state.fontcolor || '#000'}`
-            }
+        await store.dispatch(`${ACTION_COMMERCIAL_STATUS_MODULE}${ORDERSTATETAG_LOAD_ORDER_STATES}`) 
+    }
+    if(order_states.value.length > 0) {
+        const order_state = order_states.value.find(obj => obj.id == props.id)
+        if(typeof order_state != "undefined") {
+            status.value = order_state.name
+            style.value = `width:${props.width};background-color: ${order_state.color};color: ${order_state.fontcolor || '#000'}`
         }
     }
+    
 })
           
            
