12408b61a95af9af89a947c1508480dcc586a081
FireflyStars <gotodev7@gmail.com>
2022-07-15 14:49:09 +0800

    added tooltip for event description. so added tippy.js

--- resources/js/pages/ActionCommercial/Calendar.vue
+++ resources/js/pages/ActionCommercial/Calendar.vue
@@ -38,7 +38,8 @@ import dayGridPlugin  from '@fullcalendar/daygrid'
 import timeGridPlugin  from '@fullcalendar/timegrid'
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 import frLocale from '@fullcalendar/core/locales/fr';
-// import Tooltip from 'tooltip.js';
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css'; // optional for styling
 
 export default {
     name: 'ActionCommercialCalendar',
@@ -116,14 +117,16 @@ export default {
                     ttl: 5,
                 });  
             },
-            // eventDidMount: (info)=>{
-            //     var tooltip = new Tooltip(info.el, {
-            //         title: info.event.extendedProps.description,
-            //         placement: 'top',
-            //         trigger: 'hover',
-            //         container: 'body'
-            //     });
-            // },            
+            eventDidMount: (info)=>{
+              tippy(info.el, {
+                content: info.event.extendedProps.description,
+                theme: 'tomato',
+                allowHTML: true,
+                placement: 'top',
+                trigger: 'mouseenter',
+                duration: [250, 0]
+              });
+            },            
                        
         })
         return {
@@ -172,105 +175,11 @@ export default {
 .fc-daygrid-dot-event:hover{
     cursor: pointer;
 }
-// .fc .fc-daygrid-day.fc-day-today,
-// .fc-timegrid-col.fc-day-today .fc-timegrid-col-frame {
-//     background-color: #e86f29;
-//     opacity: .6;
-// }
-.popper,
-.tooltip {
-  position: absolute;
-  z-index: 9999;
-  background: #FFC107;
-  color: black;
-  width: 150px;
-  border-radius: 3px;
-  box-shadow: 0 0 2px rgba(0,0,0,0.5);
-  padding: 10px;
-  text-align: center;
+.tippy-box[data-theme~='tomato'] {
+  background-color: #e86f29;
+  color: white;
 }
-.style5 .tooltip {
-  background: #1E252B;
-  color: #FFFFFF;
-  max-width: 200px;
-  width: auto;
-  font-size: .8rem;
-  padding: .5em 1em;
-}
-.popper .popper__arrow,
-.tooltip .tooltip-arrow {
-  width: 0;
-  height: 0;
-  border-style: solid;
-  position: absolute;
-  margin: 5px;
-}
-
-.tooltip .tooltip-arrow,
-.popper .popper__arrow {
-  border-color: #FFC107;
-}
-.style5 .tooltip .tooltip-arrow {
-  border-color: #1E252B;
-}
-.popper[x-placement^="top"],
-.tooltip[x-placement^="top"] {
-  margin-bottom: 5px;
-}
-.popper[x-placement^="top"] .popper__arrow,
-.tooltip[x-placement^="top"] .tooltip-arrow {
-  border-width: 5px 5px 0 5px;
-  border-left-color: transparent;
-  border-right-color: transparent;
-  border-bottom-color: transparent;
-  bottom: -5px;
-  left: calc(50% - 5px);
-  margin-top: 0;
-  margin-bottom: 0;
-}
-.popper[x-placement^="bottom"],
-.tooltip[x-placement^="bottom"] {
-  margin-top: 5px;
-}
-.tooltip[x-placement^="bottom"] .tooltip-arrow,
-.popper[x-placement^="bottom"] .popper__arrow {
-  border-width: 0 5px 5px 5px;
-  border-left-color: transparent;
-  border-right-color: transparent;
-  border-top-color: transparent;
-  top: -5px;
-  left: calc(50% - 5px);
-  margin-top: 0;
-  margin-bottom: 0;
-}
-.tooltip[x-placement^="right"],
-.popper[x-placement^="right"] {
-  margin-left: 5px;
-}
-.popper[x-placement^="right"] .popper__arrow,
-.tooltip[x-placement^="right"] .tooltip-arrow {
-  border-width: 5px 5px 5px 0;
-  border-left-color: transparent;
-  border-top-color: transparent;
-  border-bottom-color: transparent;
-  left: -5px;
-  top: calc(50% - 5px);
-  margin-left: 0;
-  margin-right: 0;
-}
-.popper[x-placement^="left"],
-.tooltip[x-placement^="left"] {
-  margin-right: 5px;
-}
-.popper[x-placement^="left"] .popper__arrow,
-.tooltip[x-placement^="left"] .tooltip-arrow {
-  border-width: 5px 0 5px 5px;
-  border-top-color: transparent;
-  border-right-color: transparent;
-  border-bottom-color: transparent;
-  right: -5px;
-  top: calc(50% - 5px);
-  margin-left: 0;
-  margin-right: 0;
+.tippy-box[data-theme~='tomato'][data-placement^='top'] > .tippy-arrow::before {
+  border-top-color: #e86f29;
 }
 </style>
